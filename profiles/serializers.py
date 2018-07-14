c77478695009e3015f58bddcf01cc136ad61af90
Shashi Kumar P <shashiessp@gmail.com>
2018-07-14 16:07:05 -0500

    remove unused field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -12,5 +12,3 @@ class UserSerializer(serializers.ModelSerializer):
 class TrainerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trainer
-        fields = ('email', 'first_name', 'last_name', 'age', 'location',
-        		  'bio',)
